001412f775cc6aa105b9566d9f82a3e472eac834
lion-man44 <lion44man@gmail.com>
2021-09-30 13:50:17 +0900

    Modified running port

--- main.py
+++ main.py
@@ -228,4 +228,5 @@ def __profile_image(file):
 if __name__ == '__main__':
     app.secret_key = 'test'
     app.config['SESSION_TYPE'] = 'filesystem'
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug = True, host='0.0.0.0', port = port)
